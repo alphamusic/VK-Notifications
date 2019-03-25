650623bd734866891103aa722685011e5d3196ce
Stepan Novozhilov <hit2hat@icloud.com>
2019-03-25 15:06:17 +0300

    Refactoring) Delete temp variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-FINAL_MESSAGE=""
-
 if [[ -z "$VK_TOKEN" ]]; then
     echo "VK_TOKEN is required to run this action"
     exit 126
@@ -13,11 +11,9 @@ if [[ -z "$VK_USERS" ]]; then
 fi
 
 if [[ -z "$VK_MESSAGE" ]]; then
-    FINAL_MESSAGE=$'GitHub Notification 🔔\n📚 Repository:'+"$GITHUB_REPOSITORY"+$'\n🌳 Branch: '+"$GITHUB_REF"+$'\n📃 Event: '+"$GITHUB_EVENT_NAME"
-else
-    FINAL_MESSAGE="$VK_MESSAGE"
+    VK_MESSAGE=$'GitHub Notification 🔔\n📚 Repository:'+"$GITHUB_REPOSITORY"+$'\n🌳 Branch: '+"$GITHUB_REF"+$'\n📃 Event: '+"$GITHUB_EVENT_NAME"
 fi
 
-FINAL_URL="access_token=$VK_TOKEN&v=5.87&user_ids=$VK_USERS&message=$FINAL_MESSAGE"
+FINAL_URL="access_token=$VK_TOKEN&v=5.87&user_ids=$VK_USERS&message=$VK_MESSAGE"
 
 curl -d "$FINAL_URL" https://api.vk.com/method/messages.send
