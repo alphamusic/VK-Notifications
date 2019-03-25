df6958e97ea7f4ba5aac9335565937df54ad7155
Stepan Novozhilov <hit2hat@icloud.com>
2019-03-25 14:53:15 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+## VK Notification
+This action for VK Notification
+
+### Secrets
+* VK_TOKEN (Required) - access_token of group, from which messages will be sent (with "messages" scope)
+
+### Environment
+* VK_USERS (Required) - list of users id, who need to be notified. separated by ","
+* VK_Message (Optional) - text of notification
+
+##### Example
+```
+action "VK Notification" {
+    uses = "hit2hat/VK-Notifications"
+    secrets = ["VK_TOKEN"]
+    env = {
+        VK_USERS = "182625786"
+    }
+}
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+FINAL_MESSAGE=""
+
 if [[ -z "$VK_TOKEN" ]]; then
     echo "VK_TOKEN is required to run this action"
     exit 126
@@ -11,7 +13,9 @@ if [[ -z "$VK_USERS" ]]; then
 fi
 
 if [[ -z "$VK_MESSAGE" ]]; then
-    VK_MESSAGE=$'GitHub Notification 🔔\n📚 Repository:'+"$GITHUB_REPOSITORY"+$'\n🌳 Branch: ' + "$GITHUB_REF" + $'\n📃 Event: ' + "$GITHUB_EVENT_NAME"
+    FINAL_MESSAGE=$'GitHub Notification 🔔\n📚 Repository:'+"$GITHUB_REPOSITORY"+$'\n🌳 Branch: ' + "$GITHUB_REF" + $'\n📃 Event: ' + "$GITHUB_EVENT_NAME"
+else
+    FINAL_MESSAGE="$VK_MESSAGE"
 fi
 
 FINAL_URL="access_token=$VK_TOKEN&v=5.87&user_ids=$VK_USERS&message=$VK_MESSAGE"
